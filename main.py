d2d79c1b598f1890c146dd2f0cfb2f69d9b23e8f
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-06-21 11:50:32 +0300

    Точные названия
    - Изменено названия wine на product

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 WINERY_CREATED_YEAR = 1920
 
-def get_wines_list(filename):
-    wines = pandas.read_excel(filename, sheet_name='Лист1',keep_default_na=False).to_dict('records')
-    wine_categorized = collections.defaultdict(list)
-    for wine in wines:
-        wine_categorized[wine['Категория']].append(wine)
-    return wine_categorized
+def get_products_list(filename):
+    products = pandas.read_excel(filename, sheet_name='Лист1',keep_default_na=False).to_dict('records')
+    product_categorized = collections.defaultdict(list)
+    for product in products:
+        product_categorized[product['Категория']].append(product)
+    return product_categorized
 
 load_dotenv()
 product_catalog_path = os.getenv('CATALOG_PATH', default='product_catalog.xlsx')
@@ -28,7 +28,7 @@ template = env.get_template('template.html')
 winery_age = datetime.datetime.now().year - WINERY_CREATED_YEAR
 rendered_page = template.render(
     winery_age = winery_age,
-    wines = get_wines_list(product_catalog_path)
+    products_list = get_products_list(product_catalog_path)
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
