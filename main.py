61eedf5cb72352899581b86da040f9c092879234
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-06-21 11:23:44 +0300

    Путь к файлу конфигурации
    - добавлен requirements
    - путь у файлу конфигурации excel указывается как переменная окружения

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 import collections
-import datetime 
-import pandas
+import datetime
+import os
 
+import pandas
+from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 WINERY_CREATED_YEAR = 1920
 
@@ -14,7 +16,9 @@ def get_wines_list(filename):
         wine_categorized[wine['Категория']].append(wine)
     return wine_categorized
 
-
+load_dotenv()
+product_catalog_path = os.getenv('CATALOG_PATH', default='product_catalog.xlsx')
+print(product_catalog_path)
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -24,7 +28,7 @@ template = env.get_template('template.html')
 winery_age = datetime.datetime.now().year - WINERY_CREATED_YEAR
 rendered_page = template.render(
     winery_age = winery_age,
-    wines = get_wines_list('wine3.xlsx')
+    wines = get_wines_list(product_catalog_path)
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
